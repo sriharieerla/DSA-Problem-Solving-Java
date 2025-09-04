8cbb33ee6e656ca1c6fff23bb8b12b234ecc714f
sriharieerla <eerlasriharimanikanta@gmail.com>
2025-09-04 22:25:19 +0530

    Searchings

---
+++ Arrays/Searchings/TernarySearch.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class TernarySearchExample {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input array size
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        // Input array elements (must be sorted in ascending order)
+        System.out.println("Enter elements in ascending order:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Input element to search
+        System.out.print("Enter element to search: ");
+        int target = sc.nextInt();
+
+        int index = ternarySearch(arr, target);
+
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+
+        sc.close();
+    }
+
+    // Iterative Ternary Search
+    public static int ternarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid1 = left + (right - left) / 3;
+            int mid2 = right - (right - left) / 3;
+
+            if (arr[mid1] == target) return mid1;
+            if (arr[mid2] == target) return mid2;
+
+            if (target < arr[mid1]) {
+                right = mid1 - 1;
+            } else if (target > arr[mid2]) {
+                left = mid2 + 1;
+            } else {
+                left = mid1 + 1;
+                right = mid2 - 1;
+            }
+        }
+
+        return -1; // Not found
+    }
+}
