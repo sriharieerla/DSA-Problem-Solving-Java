7ce4e7876fda085615f1b37d2a7a5437330729b4
sriharieerla <eerlasriharimanikanta@gmail.com>
2025-09-09 22:02:28 +0530

    easy_sortings

---
+++ Arrays/Sortings/Easy/BubbleSort.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class BubbleSortDynamic {
+    static void bubbleSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        bubbleSort(arr);
+
+        System.out.print("Sorted array: ");
+        for (int num : arr) System.out.print(num + " ");
+    }
+}
---
+++ Arrays/Sortings/Easy/SelectionSort.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class SelectionSortDynamic {
+    static void selectionSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        selectionSort(arr);
+
+        System.out.print("Sorted array: ");
+        for (int num : arr) System.out.print(num + " ");
+
+        sc.close();
+    }
+}
