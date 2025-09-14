d21d1c03a19f797e21532056e95e82adcaf1a9dc
sriharieerla <eerlasriharimanikanta@gmail.com>
2025-09-14 20:14:47 +0530

    InsertionAtBeginning

---
+++ LinkedList/InsertionAtBeginning.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class InsertionAtBeginning {
+
+    // Insert node at the beginning
+    public static Node insertAtBeginning(Node head, int data) {
+        Node newNode = new Node(data);
+        newNode.next = head; // link new node to old head
+        head = newNode;      // update head to new node
+        return head;
+    }
+
+    // Count total nodes
+    public static int countNodes(Node head) {
+        int count = 0;
+        Node temp = head;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    // Display linked list
+    public static void display(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+
+        System.out.print("Enter number of nodes: ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            int value = sc.nextInt();
+            head = insertAtBeginning(head, value);
+        }
+
+        System.out.print("Linked List: ");
+        display(head);
+
+        int totalNodes = countNodes(head);
+        System.out.println("Total nodes in the linked list: " + totalNodes);
+
+        sc.close();
+    }
+}
